e865132616bc0d2e12ab7a7309145753e844decb
Grimm-mmirG <theisaachill@gmail.com>
2024-11-28 15:03:18 -0700

    Fixed Bug that disallowed downloads

--- App.js
+++ App.js
@@ -1,24 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
-import { View, Button, Text, StyleSheet } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator(); 
 
 export default function App() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleTheme = () => setIsDarkMode(prev => !prev);
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About">
-          {props => <AboutScreen {...props} toggleTheme={toggleTheme} isDarkMode={isDarkMode} />}
-        </Stack.Screen>
+        <Stack.Screen name="About" component={AboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['react-native-reanimated/plugin'], // Add this if you're using react-native-reanimated
+    plugins: ['react-native-reanimated/plugin'], 
   };
   
